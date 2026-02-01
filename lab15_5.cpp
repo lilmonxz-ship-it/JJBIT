c4b078ad836c5cc93cb75276b0c8ceabe06e888c
lilmonxz-ship-it <lilmonxz@gmail.com>
2026-02-01 20:42:36 +0700

    Update lab15_5.cpp

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,20 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+
+
+
+void shuffle(int *a, int *b, int *c, int *d){
+    int l[] = {*a, *b, *c, *d};
+        for(int i = 0; i < 4; i++){
+            int ran = rand() % 4;
+            int t = l[i];
+		l[i] = l[ran];
+		l[ran] = t;
+        }
+    *a = l[0];
+	*b = l[1];
+	*c = l[2];
+	*d = l[3];
+    
+}
